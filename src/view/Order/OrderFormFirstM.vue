c216a4710e9d2928f62836703a939ef37c4697cc
다베 (채도연) <coehdus@naver.com>
2023-01-10 19:27:20 +0900

    수정  - PG사 리턴 URL 수정

--- src/view/Order/OrderFormFirstM.vue
+++ src/view/Order/OrderFormFirstM.vue
@@ -38,7 +38,7 @@ export default {
 				// 결제금액
 				, Amount: this.order_info.order_price
 				// 결과수신URL
-				, rtnUrl: this.$https + this.pg_info.returnUrl
+				, rtnUrl: this.pg_info.returnUrl
 				// 상품종류
 				, ItemInfo: 1
 				// 입금통보TYPE
--- src/view/Order/OrderFormPaytus.vue
+++ src/view/Order/OrderFormPaytus.vue
@@ -42,7 +42,7 @@
 					// 주문자이메일주소
 					, ordEmail: ''
 					// 결과수신URL
-					, returnUrl: this.$https + this.pg_info.returnUrl
+					, returnUrl: this.pg_info.returnUrl
 					// notiUrl
 					, notiUrl: ''
 
